cc48b745cb3bbc21b217bd6e7011ca2fb8112173
Pemika Suebsaiaun <aueyeay@pemis-mac.local>
2024-11-03 16:40:03 +0700

    first commit

---
+++ SQLiteDemo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SQLiteDemo.Repositories;
+namespace SQLiteDemo;
+
+public partial class App : Application
+{
+	public static StudentRepository _studentRepo {get ; set;}
+	public App(StudentRepository studentRepo)
+	{
+		InitializeComponent();
+		_studentRepo = studentRepo;
+
+		//MainPage = new AppShell();
+		MainPage = new StudentPage();
+	}
+}
